d2bc37a8183faec2272e0934b51cfd0cea3b2073
BonkuBay <152313074+BonkuBay@users.noreply.github.com>
2024-06-21 17:23:53 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -10,3 +10,20 @@ function zegar() {
     setTimeout("zegar()", 1000);
 }
 zegar();
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    let backToTopBtn = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+document.getElementById("back-to-top").addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
